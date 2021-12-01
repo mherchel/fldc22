435e8ad8bafbb7abcece4914e0c3862ecd3e8864
shaal <ofer@3paces.com>
2021-12-01 05:13:41 +0000

    fix artifacts source

--- .gitpod/project-setup.sh
+++ .gitpod/project-setup.sh
@@ -6,11 +6,12 @@ fi
 cd "$GITPOD_REPO_ROOT" && ddev composer install
 
 # Load artifacts
-DDEV_ARTIFACTS=${GITPOD_WORKSPACE_CONTEXT_URL}-artifacts
+DDEV_ARTIFACTS=https://github.com/shaal/fldc22-artifacts
+mkdir -p "/tmp/${DDEV_ARTIFACTS##*/}"
 git clone "${DDEV_ARTIFACTS}" "/tmp/${DDEV_ARTIFACTS##*/}" || true
 if [ -d "/tmp/${DDEV_ARTIFACTS##*/}" ]; then
     ddev import-db --src=/tmp/"${DDEV_ARTIFACTS##*/}"/db.sql.gz
-    ddev import-files --src=/tmp/"${DDEV_ARTIFACTS##*/}"/files.tgz
+    # ddev import-files --src=/tmp/"${DDEV_ARTIFACTS##*/}"/files.tgz
 fi
 
 cd "$GITPOD_REPO_ROOT" && ddev drush updb -y
