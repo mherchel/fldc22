7780389d295a914866cf20f170e30d6c2f306063
Mike Herchel <mike@herchel.com>
2023-05-16 10:00:35 -0400

    WIP on gulp file.

--- web/themes/pubsub/Gulpfile.js
+++ web/themes/pubsub/Gulpfile.js
@@ -5,39 +5,50 @@ const autoprefixer = require('autoprefixer');
 const pxtorem = require('postcss-pxtorem');
 const del = require('del');
 
-gulp.task('sass', function () {
-  return gulp.src(['./sass/**/*.scss', './components/**/*.scss'])
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer(),
-      pxtorem({
-        replace: true,
-        propList: [
-          '*',
-          '!background-position',
-          '!border',
-          '!border-width',
-          '!box-shadow',
-          '!border-top*',
-          '!border-right*',
-          '!border-bottom*',
-          '!border-left*',
-          '!border-start*',
-          '!border-end*',
-          '!outline*',
-        ],
-        mediaQuery: true,
-        minPixelValue: 3,
-      })
-    ]))
-    .pipe(gulp.dest('./css'));
+function doSassyShit(stream) {
+  return stream.pipe(sass().on('error', sass.logError))
+  .pipe(postcss([
+    autoprefixer(),
+    pxtorem({
+      replace: true,
+      propList: [
+        '*',
+        '!background-position',
+        '!border',
+        '!border-width',
+        '!box-shadow',
+        '!border-top*',
+        '!border-right*',
+        '!border-bottom*',
+        '!border-left*',
+        '!border-start*',
+        '!border-end*',
+        '!outline*',
+      ],
+      mediaQuery: true,
+      minPixelValue: 3,
+    })
+  ]));
+}
+
+gulp.task('compile-sass-directory-sass', function () {
+  return doSassyShit(gulp.src(['./sass/**/*.scss'])).pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function (done) {
-  gulp.watch(['./sass/**/*.scss', './components/**/*.scss'], gulp.series(['clean:css', 'sass']));
+gulp.task('compile-component-directory-sass', function () {
+  return doSassyShit(gulp.src(['./components/**/*.scss'])).pipe(gulp.dest('./components'));
+});
+
+gulp.task('watch', function (done) {
+  gulp.watch(['./sass/**/*.scss'], gulp.series(['clean:css-directory-css', 'compile-sass-directory-sass']));
+  gulp.watch(['./components/**/*.scss'], gulp.series(['clean:component-directory-css', 'compile-component-directory-sass']));
   done();
 });
 
-gulp.task('clean:css', function () {
+gulp.task('clean:css-directory-css', function () {
+  return del('css/**', { force: true });
+});
+
+gulp.task('clean:component-directory-css', function () {
   return del('css/**', { force: true });
 });
